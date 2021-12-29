8e060998ebcd2cf9dd1ea38aac958f9265899f3b
RyoDagg <mannai.abdlkader@gmail.com>
2021-12-29 15:27:39 +0100

    Adding features to a model using inheritance

--- models/res_partner.py
+++ models/res_partner.py
@@ -12,3 +12,12 @@ class ResPartner(models.Model):
         string='Authered Books',
         #relation = '$relation_name'
         )
+
+    count_books = fields.Integer(
+        string="Number Of Authered Books",
+        compute = '_compute_count_books')
+
+    @api.depends('authored_book_ids')
+    def _compute_count_books(self):
+        for record in self:
+            record.count_books = len(record.authored_book_ids)
